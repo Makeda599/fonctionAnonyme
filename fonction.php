6c71292c39542bd135255fa6b11ec72724af5ebb
makeda <adm670in@gmail.com>
2026-02-24 05:16:23 +0000

    soustraction

--- fonction.php
+++ fonction.php
@@ -4,3 +4,12 @@ function calculer(int $a,int $b,callable $operation){
     return $operation($a ,$b);
 }
 
+function($a,$b )use ($nomOperation){
+
+    switch($nomOperation){
+        case "soustraction":
+            return $a - $b;
+            break;
+
+    }
+};
